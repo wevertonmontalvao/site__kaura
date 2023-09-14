cda631ff38e53b854a75caa833357747ae1ad2d1
Weverton <wevertonmontalvao993@gmail.com>
2023-09-13 21:58:23 -0300

    Barra de navegação fixada

--- assets/js/main.js
+++ assets/js/main.js
@@ -80,7 +80,7 @@
   let selectHeader = select('#header')
   if (selectHeader) {
     const headerScrolled = () => {
-      if (window.scrollY > 100) {
+      if (window.scrollY > -1) {
         selectHeader.classList.add('header-scrolled')
       } else {
         selectHeader.classList.remove('header-scrolled')
